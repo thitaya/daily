3bfd297d431119781634bd7ce4deb9b2c523fb63
thitaya <tsuaimak@gmail.com>
2018-05-30 17:30:42 +0700

    เพิ่ม

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import Style from './App.css'
+import Header from './Header.js'
+import Contents from './Contents.js'
+
+
+class App extends Component {
+  render() {
+    return (
+      <div >
+        <Header />
+        <Header />
+        <Header />
+        
+        <Contents />
+      </div>
+    );
+  }
+}
+
+export default App;
